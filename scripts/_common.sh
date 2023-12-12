a07e5dcea143d0a6502400c7f8bbbaba89caca18
Laurent Gay <laurent@sleto.fr>
2023-12-12 13:07:45 +0100

    correct

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -24,7 +24,8 @@ fi
 
 function refresh_collect()
 {
-    python3 manage_${app}.py collectstatic --noinput -l
+    pushd $install_dir
+    venv/bin/python3 manage_${app}.py collectstatic --noinput -l
     rm -rf ${app}/static/static
     rm -rf ${app}/static/plugins
     rm -rf ${app}/static/tmp
@@ -34,4 +35,5 @@ function refresh_collect()
     rm -rf ${app}/static/settings.py
     rm -rf ${app}/static/django_error.log
     rm -rf ${app}/static/__init__.py
+    popd
 }
