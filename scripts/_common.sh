92a4f506e558305e6fb0952729d905b18f24a4f3
Laurent Gay <laurent@sleto.fr>
2023-12-12 14:06:16 +0100

    correct

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,12 +9,12 @@ MODULES="lucterios.contacts,lucterios.documents,lucterios.mailing"
 DATABASE="postgresql:name=$db_name,user=$db_user,password=$db_pwd,host=localhost"
 if [ "$lct_appli" == "asso" ]
 then
-    MODULES="lucterios.contacts,lucterios.documents,lucterios.mailing,diacamma,diacamma.accounting,diacamma.payoff,diacamma.invoice,diacamma.member,diacamma.event"
+    MODULES="lucterios.contacts,lucterios.documents,lucterios.mailing,diacamma.accounting,diacamma.payoff,diacamma.invoice,diacamma.member,diacamma.event"
     APPLITYPE="diacamma.asso"
 fi
 if [ "$lct_appli" == "syndic" ]
 then
-    MODULES="lucterios.contacts,lucterios.documents,lucterios.mailing,diacamma,diacamma.accounting,diacamma.payoff,diacamma.condominium"
+    MODULES="lucterios.contacts,lucterios.documents,lucterios.mailing,diacamma.accounting,diacamma.payoff,diacamma.condominium"
     APPLITYPE="diacamma.syndic"
 fi
 
@@ -25,7 +25,7 @@ fi
 function refresh_collect()
 {
     pushd $install_dir
-    venv/bin/python3 manage_${app}.py collectstatic --noinput -l
+    venv/bin/python3 manage_inst-${app}.py collectstatic --noinput -l
     rm -rf ${app}/static/static
     rm -rf ${app}/static/plugins
     rm -rf ${app}/static/tmp
