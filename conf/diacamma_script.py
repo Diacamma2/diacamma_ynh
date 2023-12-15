fb7cda225d2c70966e98041e3e72c64e440623b2
Laurent GAY <l.gay@sd-libre.fr>
2023-12-15 16:58:51 +0100

    corrections

---
+++ conf/diacamma_script.py
@@ -0,0 +1,4 @@
+from lucterios.CORE.parameters import Params
+
+Params.setvalue('mailing-smtpserver', 'localhost')
+Params.setvalue('mailing-smtpport', 1025)
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -26,12 +26,12 @@ function refresh_collect()
 {
     pushd $install_dir
     venv/bin/python3 manage_inst-${app}.py collectstatic --noinput -l
-    rm -rf inst-${app}/static/static
-    rm -rf inst-${app}/static/tmp
-    rm -rf inst-${app}/static/usr
-    rm -rf inst-${app}/static/__pycache__
-    rm -rf inst-${app}/static/settings.py
-    rm -rf inst-${app}/static/__init__.py
+    ynh_secure_remove inst-${app}/static/static
+    ynh_secure_remove inst-${app}/static/tmp
+    ynh_secure_remove inst-${app}/static/usr
+    ynh_secure_remove inst-${app}/static/__pycache__
+    ynh_secure_remove inst-${app}/static/settings.py
+    ynh_secure_remove inst-${app}/static/__init__.py
     chown -R ${app}:www-data .
     chmod 750 .
     popd
@@ -40,11 +40,7 @@ function refresh_collect()
 function check_params()
 {
     pushd $install_dir
-    echo "
-from lucterios.CORE.parameters import Params
-Params.setvalue('mailing-smtpserver', 'localhost')
-Params.setvalue('mailing-smtpport', 1025)
-" > /tmp/diacamma_script.py
+    ynh_add_config --template="../conf/diacamma_script.py" --destination="/tmp/diacamma_script.py"
     venv/bin/python3 manage_inst-${app}.py shell < /tmp/diacamma_script.py
     venv/bin/lucterios_admin.py security -n inst-${app} -e "MODE=0"
     popd
